9e4e6f1a6efdeb334d0115e8859ae85013112474
Ajhar27 <ajharkhan48@gmail.com>
2021-06-19 00:01:51 +0530

    Add Book Controller In Application

---
+++ BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+       public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with id = {id}";
+        }
+
+        public string SearchBook(string bookName,string authorName)
+        {
+            return $"hello book Name is {bookName} & Author is Mr. {authorName}";
+        }
+    }
+}
